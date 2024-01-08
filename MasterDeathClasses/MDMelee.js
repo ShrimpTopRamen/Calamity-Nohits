1e14536f14b11b2f2d7e1f1dcafb868f4faa435d
ShrimpTopRamen <rsosa322626@gmail.com>
2024-01-07 20:32:13 -0800

    handlebars.js is epic and cool

---
+++ MasterDeathClasses/MDMelee.js
@@ -0,0 +1,267 @@
+var bossesData = [
+  {
+    bossId: "KingSlime",
+    bossName: "King Slime",
+    bossIcon: "../bossIcons/Resize/Map_Icon_King_Slime.png",
+    categories: [
+      {
+        categoryName: "Armour",
+        items: [
+          {
+            cssClass: "head",
+            imageUrl: "../armor/final/Gold_Helmet.png",
+            itemName: "Gold Helmet",
+          },
+          {
+            cssClass: "chest",
+            imageUrl: "../armor/final/Gold_Chainmail.png",
+            itemName: "Gold Chainmail",
+          },
+          {
+            cssClass: "legs",
+            imageUrl: "../armor/final/Gold_Greaves.png",
+            itemName: "Gold Greaves",
+          },
+        ],
+        armorName: "Gold Armour",
+      },
+      {
+        categoryName: "Weapon(s)",
+        items: [
+          {
+            cssClass: "weapons",
+            imageUrl: "../weapons/final/Enchanted_Sword_(item).png",
+            itemName: "Enchanted Sword",
+          },
+        ],
+        weaponName: "Enchanted Sword",
+      },
+      {
+        categoryName: "Accessories",
+        items: [
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Shark_Tooth_Necklace.png",
+            itemName: "Shark Tooth Necklace",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Sandstorm_in_a_Bottle.png",
+            itemName: "Sandstorm in a Bottle",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Cloud_in_a_Bottle.png",
+            itemName: "Cloud in a Bottle",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Feral_Claws.png",
+            itemName: "Feral Claws",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Fart_in_a_Jar.png",
+            itemName: "Fart in a Jar",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Spectre_Boots.png",
+            itemName: "Spectre Boots",
+          },
+        ],
+      },
+    ],
+    moreInformationHeader: "More Information",
+    videoSrc: "https://www.youtube.com/embed/zdnORlKG86U",
+    videoCaption: "YouTube Video",
+    calamityVersion: "Calamity Version: 2.0.1.003",
+    terrariaVersion: "Terraria Version: 1.4.3.6",
+    timeTaken: "Time Taken: 56 seconds",
+    mnl: "MNL = 35 seconds",
+    met: "Met = True",
+    comments: "Comments: Like and Subscribe",
+  },
+
+  {
+    bossId: "DesertScourge",
+    bossName: "Desert Scourge",
+    bossIcon: "../bossIcons/Resize/Desert_Scourge_map.png",
+    categories: [
+      {
+        categoryName: "Armour",
+        items: [
+          {
+            cssClass: "head",
+            imageUrl: "../armor/final/Platinum_Helmet.png",
+            itemName: "Platinum Helmet",
+          },
+          {
+            cssClass: "chest",
+            imageUrl: "../armor/final/Platinum_Chainmail.png",
+            itemName: "Platinum Chainmail",
+          },
+          {
+            cssClass: "legs",
+            imageUrl: "../armor/final/Platinum_Greaves.png",
+            itemName: "Platinum Greaves",
+          },
+        ],
+        armorName: "Platinum Armour",
+      },
+      {
+        categoryName: "Weapon(s)",
+        items: [
+          {
+            cssClass: "weapons",
+            imageUrl: "../weapons/final/Enchanted_Sword_(item).png",
+            itemName: "Enchanted Sword",
+          },
+          {
+            cssClass: "weapons",
+            imageUrl: "../weapons/final/Starfury.png",
+            itemName: "Starfury",
+          },
+        ],
+      },
+      {
+        categoryName: "Accessories",
+        items: [
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Shark_Tooth_Necklace.png",
+            itemName: "Shark Tooth Necklace",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Sandstorm_in_a_Bottle.png",
+            itemName: "Sandstorm in a Bottle",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Fledgling_Wings.png",
+            itemName: "Fledgling Wings",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Feral_Claws.png",
+            itemName: "Feral Claws",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Cloud_in_a_Bottle.png",
+            itemName: "Cloud in a Bottle",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Spectre_Boots.png",
+            itemName: "Spectre Boots",
+          },
+        ],
+      },
+    ],
+    moreInformationHeader: "More Information",
+    videoSrc: "https://www.youtube.com/embed/NItBMNSDQdE",
+    videoCaption: "YouTube Video",
+    calamityVersion: "Calamity Version: 2.0.1.003",
+    terrariaVersion: "Terraria Version: 1.4.3.6",
+    timeTaken: "Time Taken: 37 seconds",
+    mnl: "MNL = 35 seconds",
+    met: "Met = True",
+    comments: "Comments: uhhhh",
+  },
+  {
+    bossId: "EoC",
+    bossName: "Eye of Cthulhu",
+    bossIcon: "../bossIcons/Resize/Map_Icon_Eye_of_Cthulhu_(first_form).png",
+    categories: [
+      {
+        categoryName: "Armour",
+        items: [
+          {
+            cssClass: "head",
+            imageUrl: "../armor/final/Platinum_Helmet.png",
+            itemName: "Platinum Helmet",
+          },
+          {
+            cssClass: "chest",
+            imageUrl: "../armor/final/Platinum_Chainmail.png",
+            itemName: "Platinum Chainmail",
+          },
+          {
+            cssClass: "legs",
+            imageUrl: "../armor/final/Platinum_Greaves.png",
+            itemName: "Platinum Greaves",
+          },
+        ],
+        armorName: "Platinum Armour",
+      },
+      {
+        categoryName: "Weapon(s)",
+        items: [
+          {
+            cssClass: "weapons",
+            imageUrl: "../weapons/final/Enchanted_Sword_(item).png",
+            itemName: "Enchanted Sword",
+          },
+        ],
+      },
+      {
+        categoryName: "Accessories",
+        items: [
+          {
+
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Feral_Claws.png",
+            itemName: "Feral Claws",
+
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Sandstorm_in_a_Bottle.png",
+            itemName: "Sandstorm in a Bottle",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Fledgling_Wings.png",
+            itemName: "Fledgling Wings",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Shark_Tooth_Necklace.png",
+            itemName: "Shark Tooth Necklace",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Fart_in_a_Jar.png",
+            itemName: "Fart in a Jar",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Frostspark_Boots.png",
+            itemName: "Frostspark Boots",
+          },
+        ],
+      },
+    ],
+    moreInformationHeader: "More Information",
+    videoSrc: "https://www.youtube.com/embed/Ed54iR7VmyU",
+    videoCaption: "YouTube Video",
+    calamityVersion: "Calamity Version: 2.0.1.003",
+    terrariaVersion: "Terraria Version: 1.4.3.6",
+    timeTaken: "Time Taken: 1:05",
+    mnl: "MNL = 45 seconds",
+    met: "Met = True",
+    comments: "no ft. Toasty's Calamity QoL Mod",
+  },
+];
+
+function renderBosses() {
+  var templateScript = document.getElementById("boss-template").innerHTML;
+  var template = Handlebars.compile(templateScript);
+
+  bossesData.forEach(function (boss) {
+    var bossHtml = template(boss);
+    document.body.insertAdjacentHTML("beforeend", bossHtml);
+  });
+}
