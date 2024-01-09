fb8644b5e7cfccee5ba5910215cf193767119d18
ShrimpTopRamen <rsosa322626@gmail.com>
2024-01-08 21:33:56 -0800

    fully added Rogue, now data entry time

---
+++ MasterDeathClasses/MDRogue.js
@@ -0,0 +1,359 @@
+var bossesData = [
+    {
+      bossId: "KingSlime",
+      bossName: "King Slime",
+      bossIcon: "../bossIcons/Resize/Map_Icon_King_Slime.png",
+      categories: [
+        {
+          categoryName: "Armour",
+          items: [
+            {
+              cssClass: "head",
+              imageUrl: "../armor/final/Snow_Ruffian_Mask.png",
+              itemName: "Snow Ruffian Mask",
+            },
+            {
+              cssClass: "chest",
+              imageUrl: "../armor/final/Snow_Ruffian_Chestplate.png",
+              itemName: "Snow Ruffian Chestplate",
+            },
+            {
+              cssClass: "legs",
+              imageUrl: "../armor/final/Snow_Ruffian_Greaves.png",
+              itemName: "Snow Ruffian Greaves",
+            },
+          ],
+        },
+        {
+          categoryName: "Weapon(s)",
+          items: [
+            {
+              cssClass: "weapons",
+              imageUrl: "../weapons/final/Gilded_Dagger.png",
+              itemName: "Gilded Dagger",
+            },
+            {
+              cssClass: "weapons",
+              imageUrl: "../weapons/final/Crystalline.png",
+              itemName: "Crystalline",
+            },
+          ],
+        },
+        {
+          categoryName: "Accessories",
+          items: [
+            {
+              cssClass: "accessories",
+              imageUrl: "../accessories/final/Scuttler's_Jewel.png",
+              itemName: "Scuttler's Jewel",
+            },
+            {
+              cssClass: "accessories",
+              imageUrl: "../accessories/final/Sandstorm_in_a_Bottle.png",
+              itemName: "Sandstorm in a Bottle",
+            },
+            {
+              cssClass: "accessories",
+              imageUrl: "../accessories/final/Fledgling_Wings.png",
+              itemName: "Fledgling Wings",
+            },
+            {
+              cssClass: "accessories",
+              imageUrl: "../accessories/final/Feral_Claws.png",
+              itemName: "Feral Claws",
+            },
+            {
+              cssClass: "accessories",
+              imageUrl: "../accessories/final/Fart_in_a_Jar.png",
+              itemName: "Fart in a Jar",
+            },
+            {
+              cssClass: "accessories",
+              imageUrl: "../accessories/final/Spectre_Boots.png",
+              itemName: "Spectre Boots",
+            },
+          ],
+        },
+      ],
+      moreInformationHeader: "More Information",
+      videoSrc:
+        "https://www.youtube.com/embed/zdnORlKG86U?si=uP8MC1to-mtH1P0W&amp;start=288",
+      videoCaption: "YouTube Video",
+      calamityVersion: "Calamity Version: 2.0.1.003",
+      terrariaVersion: "Terraria Version: 1.4.3.6",
+      timeTaken: "Time Taken: 45 seconds",
+      mnl: "MNL = 35 seconds",
+      met: "Met = True",
+      comments:
+        "Comments: Like and Subscribe (Rogue Editon)",
+    },
+  
+    {
+      bossId: "DesertScourge",
+      bossName: "Desert Scourge",
+      bossIcon: "../bossIcons/Resize/Desert_Scourge_map.png",
+      categories: [
+        {
+            categoryName: "Armour",
+            items: [
+              {
+                cssClass: "head",
+                imageUrl: "../armor/final/Snow_Ruffian_Mask.png",
+                itemName: "Snow Ruffian Mask",
+              },
+              {
+                cssClass: "chest",
+                imageUrl: "../armor/final/Snow_Ruffian_Chestplate.png",
+                itemName: "Snow Ruffian Chestplate",
+              },
+              {
+                cssClass: "legs",
+                imageUrl: "../armor/final/Snow_Ruffian_Greaves.png",
+                itemName: "Snow Ruffian Greaves",
+              },
+            ],
+          },
+          {
+            categoryName: "Weapon(s)",
+            items: [
+              {
+                cssClass: "weapons",
+                imageUrl: "../weapons/final/Gilded_Dagger.png",
+                itemName: "Gilded Dagger",
+              },
+              {
+                cssClass: "weapons",
+                imageUrl: "../weapons/final/Crystalline.png",
+                itemName: "Crystalline",
+              },
+            ],
+          },
+        {
+          categoryName: "Accessories",
+          items: [
+            {
+                cssClass: "accessories",
+                imageUrl: "../accessories/final/Scuttler's_Jewel.png",
+                itemName: "Scuttler's Jewel",
+              },
+              {
+                cssClass: "accessories",
+                imageUrl: "../accessories/final/Sandstorm_in_a_Bottle.png",
+                itemName: "Sandstorm in a Bottle",
+              },
+              {
+                cssClass: "accessories",
+                imageUrl: "../accessories/final/Fledgling_Wings.png",
+                itemName: "Fledgling Wings",
+              },
+              {
+                cssClass: "accessories",
+                imageUrl: "../accessories/final/Shark_Tooth_Necklace.png",
+                itemName: "Shark Tooth Necklace",
+              },
+              {
+                cssClass: "accessories",
+                imageUrl: "../accessories/final/Fart_in_a_Jar.png",
+                itemName: "Fart in a Jar",
+              },
+              {
+                cssClass: "accessories",
+                imageUrl: "../accessories/final/Frostspark_Boots.png",
+                itemName: "Frostspark Boots",
+              },
+          ],
+        },
+      ],
+      moreInformationHeader: "More Information",
+      videoSrc:
+        "https://www.youtube.com/embed/NItBMNSDQdE?si=L-RAJUc4PXku4fCX&amp;start=335",
+      videoCaption: "YouTube Video",
+      calamityVersion: "Calamity Version: 2.0.1.003",
+      terrariaVersion: "Terraria Version: 1.4.3.6",
+      timeTaken: "Time Taken: 1:28",
+      mnl: "MNL = 35 seconds",
+      met: "Met = True",
+      comments: "Comments: slowest i think",
+    },
+    {
+      bossId: "EoC",
+      bossName: "Eye of Cthulhu",
+      bossIcon: "../bossIcons/Resize/Map_Icon_Eye_of_Cthulhu_(first_form).png",
+      categories: [
+        {
+          categoryName: "Armour",
+          items: [
+            {
+                cssClass: "head",
+                imageUrl: "../armor/final/Snow_Ruffian_Mask.png",
+                itemName: "Snow Ruffian Mask",
+              },
+              {
+                cssClass: "chest",
+                imageUrl: "../armor/final/Snow_Ruffian_Chestplate.png",
+                itemName: "Snow Ruffian Chestplate",
+              },
+              {
+                cssClass: "legs",
+                imageUrl: "../armor/final/Snow_Ruffian_Greaves.png",
+                itemName: "Snow Ruffian Greaves",
+              },
+          ],
+        },
+        {
+          categoryName: "Weapon(s)",
+          items: [
+            {
+                cssClass: "weapons",
+                imageUrl: "../weapons/final/Crystalline.png",
+                itemName: "Crystalline",
+              },
+          ],
+        },
+        {
+          categoryName: "Accessories",
+          items: [
+            {
+                cssClass: "accessories",
+                imageUrl: "../accessories/final/Scuttler's_Jewel.png",
+                itemName: "Scuttler's Jewel",
+              },
+              {
+                cssClass: "accessories",
+                imageUrl: "../accessories/final/Sandstorm_in_a_Bottle.png",
+                itemName: "Sandstorm in a Bottle",
+              },
+              {
+                cssClass: "accessories",
+                imageUrl: "../accessories/final/Fledgling_Wings.png",
+                itemName: "Fledgling Wings",
+              },
+              {
+                cssClass: "accessories",
+                imageUrl: "../accessories/final/Shark_Tooth_Necklace.png",
+                itemName: "Shark Tooth Necklace",
+              },
+              {
+                cssClass: "accessories",
+                imageUrl: "../accessories/final/Fart_in_a_Jar.png",
+                itemName: "Fart in a Jar",
+              },
+              {
+                cssClass: "accessories",
+                imageUrl: "../accessories/final/Frostspark_Boots.png",
+                itemName: "Frostspark Boots",
+              },
+          ],
+        },
+      ],
+      moreInformationHeader: "More Information",
+      videoSrc:
+        "https://www.youtube.com/embed/Ed54iR7VmyU?si=zVL4gaucSDIbr_ua&amp;start=352",
+      videoCaption: "YouTube Video",
+      calamityVersion: "Calamity Version: 2.0.1.003",
+      terrariaVersion: "Terraria Version: 1.4.3.6",
+      timeTaken: "Time Taken: 42 seconds",
+      mnl: "MNL = 45 seconds",
+      met: "Met = True",
+      comments: "Comments: stealth strike barely made me miss mnl methinks",
+    },
+  
+    {
+      bossId: "Crabulon",
+      bossName: "Crabulon",
+      bossIcon: "../bossIcons/Resize/Crabulon_map.png",
+      categories: [
+        {
+          categoryName: "Armour",
+          items: [
+            {
+              cssClass: "head",
+              imageUrl: "../armor/final/Sulphurous_Helmet.png",
+              itemName: "Sulphurous Helmet",
+            },
+            {
+              cssClass: "chest",
+              imageUrl: "../armor/final/Sulphurous_Breastplate.png",
+              itemName: "Sulphurous Breastplate",
+            },
+            {
+              cssClass: "legs",
+              imageUrl: "../armor/final/Sulphurous_Leggings.png",
+              itemName: "Sulphurous Leggings",
+            },
+          ],
+        },
+        {
+          categoryName: "Weapon(s)",
+          items: [
+            {
+                cssClass: "weapons",
+                imageUrl: "../weapons/final/Crystalline.png",
+                itemName: "Crystalline",
+              },
+              {
+                cssClass: "weapons",
+                imageUrl: "../weapons/final/Contaminated_Bile.png",
+                itemName: "Contaminated Bile",
+              },
+          ],
+        },
+        {
+          categoryName: "Accessories",
+          items: [
+            {
+                cssClass: "accessories",
+                imageUrl: "../accessories/final/Scuttler's_Jewel.png",
+                itemName: "Scuttler's Jewel",
+              },
+              {
+                cssClass: "accessories",
+                imageUrl: "../accessories/final/Fledgling_Wings.png",
+                itemName: "Fledgling Wings",
+              },
+              {
+                cssClass: "accessories",
+                imageUrl: "../accessories/final/Coin_of_Deceit.png",
+                itemName: "Coin of Deceit",
+              },
+              {
+                cssClass: "accessories",
+                imageUrl: "../accessories/final/Shark_Tooth_Necklace.png",
+                itemName: "Shark Tooth Necklace",
+              },
+              {
+                cssClass: "accessories",
+                imageUrl: "../accessories/final/Fart_in_a_Jar.png",
+                itemName: "Fart in a Jar",
+              },
+              {
+                cssClass: "accessories",
+                imageUrl: "../accessories/final/Frostspark_Boots.png",
+                itemName: "Frostspark Boots",
+              },
+          ],
+        },
+      ],
+      moreInformationHeader: "More Information",
+      videoSrc:
+        "https://www.youtube.com/embed/rqDe5ZGxim8?si=tnhRJIvGDXNKERm0&amp;start=303",
+      videoCaption: "YouTube Video",
+      calamityVersion: "Calamity Version: 2.0.1.3",
+      terrariaVersion: "Terraria Version: 1.4.3.6",
+      timeTaken: "Time Taken: 1:11",
+      mnl: "MNL = 45 seconds",
+      met: "Met = True",
+      comments: "Comments: EoC dead means acid rain event can start",
+    },
+  ];
+  
+  function renderBosses() {
+    var templateScript = document.getElementById("boss-template").innerHTML;
+    var template = Handlebars.compile(templateScript);
+  
+    bossesData.forEach(function (boss) {
+      var bossHtml = template(boss);
+      document.body.insertAdjacentHTML("beforeend", bossHtml);
+    });
+  }
+  
