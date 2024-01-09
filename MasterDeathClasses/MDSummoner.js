0d6dcb2194a6ac83cb1f5da74bf4a2d5c4c6872d
ShrimpTopRamen <rsosa322626@gmail.com>
2024-01-08 21:07:44 -0800

    fully added summoner.js

---
+++ MasterDeathClasses/MDSummoner.js
@@ -0,0 +1,383 @@
+var bossesData = [
+  {
+    bossId: "KingSlime",
+    bossName: "King Slime",
+    bossIcon: "../bossIcons/Resize/Map_Icon_King_Slime.png",
+    categories: [
+      {
+        categoryName: "Armour",
+        items: [
+          {
+            cssClass: "head",
+            imageUrl: "../armor/final/Wulfrum_Hat_&_Goggles.png",
+            itemName: "Wulfrum Hat & Goggles",
+          },
+          {
+            cssClass: "chest",
+            imageUrl: "../armor/final/Wulfrum_Jacket.png",
+            itemName: "Wulfrum Jacket",
+          },
+          {
+            cssClass: "legs",
+            imageUrl: "../armor/final/Wulfrum_Overalls.png",
+            itemName: "Wulfrum Overalls",
+          },
+        ],
+      },
+      {
+        categoryName: "Weapon(s)",
+        items: [
+          {
+            cssClass: "weapons",
+            imageUrl: "../weapons/final/Snapthorn.png",
+            itemName: "Snapthorn",
+          },
+          {
+            cssClass: "weapons",
+            imageUrl: "../weapons/final/Wulfrum_Controller.png",
+            itemName: "Wulfrum Controller",
+          },
+        ],
+      },
+      {
+        categoryName: "Accessories",
+        items: [
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Spirit_Glyph.png",
+            itemName: "Spirit Glyph",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Sandstorm_in_a_Bottle.png",
+            itemName: "Sandstorm in a Bottle",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Fledgling_Wings.png",
+            itemName: "Fledgling Wings",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Feral_Claws.png",
+            itemName: "Feral Claws",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Fart_in_a_Jar.png",
+            itemName: "Fart in a Jar",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Spectre_Boots.png",
+            itemName: "Spectre Boots",
+          },
+        ],
+      },
+    ],
+    moreInformationHeader: "More Information",
+    videoSrc:
+      "https://www.youtube.com/embed/zdnORlKG86U?si=o21e5lzo_1NaWz9W&amp;start=202",
+    videoCaption: "YouTube Video",
+    calamityVersion: "Calamity Version: 2.0.1.003",
+    terrariaVersion: "Terraria Version: 1.4.3.6",
+    timeTaken: "Time Taken: 59 seconds",
+    mnl: "MNL = 35 seconds",
+    met: "Met = True",
+    comments:
+      "Comments: Like and Subscribe (Summoner Editon) Wulfrum armor is pretty cool",
+  },
+
+  {
+    bossId: "DesertScourge",
+    bossName: "Desert Scourge",
+    bossIcon: "../bossIcons/Resize/Desert_Scourge_map.png",
+    categories: [
+      {
+        categoryName: "Armour",
+        items: [
+          {
+            cssClass: "head",
+            imageUrl: "../armor/final/Wulfrum_Hat_&_Goggles.png",
+            itemName: "Wulfrum Hat & Goggles",
+          },
+          {
+            cssClass: "chest",
+            imageUrl: "../armor/final/Wulfrum_Jacket.png",
+            itemName: "Wulfrum Jacket",
+          },
+          {
+            cssClass: "legs",
+            imageUrl: "../armor/final/Wulfrum_Overalls.png",
+            itemName: "Wulfrum Overalls",
+          },
+        ],
+      },
+      {
+        categoryName: "Weapon(s)",
+        items: [
+          {
+            cssClass: "weapons",
+            imageUrl: "../weapons/final/Snapthorn.png",
+            itemName: "Snapthorn",
+          },
+          {
+            cssClass: "weapons",
+            imageUrl: "../weapons/final/Belladonna_Spirit_Staff.png",
+            itemName: "Belladonna Spirit Staff",
+          },
+          {
+            cssClass: "weapons",
+            imageUrl: "../weapons/final/Sun_Spirit_Staff.png",
+            itemName: "Sun Spirit Staff",
+          },
+          {
+            cssClass: "weapons",
+            imageUrl: "../weapons/final/Frost_Blossom_Staff.png",
+            itemName: "Frost Blossom Staff",
+          },
+        ],
+      },
+      {
+        categoryName: "Accessories",
+        items: [
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Feral_Claws.png",
+            itemName: "Feral Claws",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Sandstorm_in_a_Bottle.png",
+            itemName: "Sandstorm in a Bottle",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Fledgling_Wings.png",
+            itemName: "Fledgling Wings",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Wulfrum_Battery.png",
+            itemName: "Wulfrum Battery",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Fart_in_a_Jar.png",
+            itemName: "Fart in a Jar",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Frostspark_Boots.png",
+            itemName: "Frostspark Boots",
+          },
+        ],
+      },
+    ],
+    moreInformationHeader: "More Information",
+    videoSrc:
+      "https://www.youtube.com/embed/NItBMNSDQdE?si=YTgpYqMDEoh9H0Gb&amp;start=121",
+    videoCaption: "YouTube Video",
+    calamityVersion: "Calamity Version: 2.0.1.003",
+    terrariaVersion: "Terraria Version: 1.4.3.6",
+    timeTaken: "Time Taken: 1:17",
+    mnl: "MNL = 35 seconds",
+    met: "Met = True",
+    comments: "Comments: none",
+  },
+  {
+    bossId: "EoC",
+    bossName: "Eye of Cthulhu",
+    bossIcon: "../bossIcons/Resize/Map_Icon_Eye_of_Cthulhu_(first_form).png",
+    categories: [
+      {
+        categoryName: "Armour",
+        items: [
+          {
+            cssClass: "head",
+            imageUrl: "../armor/final/Wulfrum_Hat_&_Goggles.png",
+            itemName: "Wulfrum Hat & Goggles",
+          },
+          {
+            cssClass: "chest",
+            imageUrl: "../armor/final/Wulfrum_Jacket.png",
+            itemName: "Wulfrum Jacket",
+          },
+          {
+            cssClass: "legs",
+            imageUrl: "../armor/final/Wulfrum_Overalls.png",
+            itemName: "Wulfrum Overalls",
+          },
+        ],
+      },
+      {
+        categoryName: "Weapon(s)",
+        items: [
+          {
+            cssClass: "weapons",
+            imageUrl: "../weapons/final/Snapthorn.png",
+            itemName: "Snapthorn",
+          },
+          {
+            cssClass: "weapons",
+            imageUrl: "../weapons/final/Frost_Blossom_Staff.png",
+            itemName: "Frost Blossom Staff",
+          },
+          {
+            cssClass: "weapons",
+            imageUrl: "../weapons/final/Sun_Spirit_Staff.png",
+            itemName: "Sun Spirit Staff",
+          },
+          {
+            cssClass: "weapons",
+            imageUrl: "../weapons/final/Wulfrum_Controller.png",
+            itemName: "Wulfrum Controller",
+          },
+        ],
+      },
+      {
+        categoryName: "Accessories",
+        items: [
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Feral_Claws.png",
+            itemName: "Feral Claws",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Sandstorm_in_a_Bottle.png",
+            itemName: "Sandstorm in a Bottle",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Fledgling_Wings.png",
+            itemName: "Fledgling Wings",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Wulfrum_Battery.png",
+            itemName: "Wulfrum Battery",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Fart_in_a_Jar.png",
+            itemName: "Fart in a Jar",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Frostspark_Boots.png",
+            itemName: "Frostspark Boots",
+          },
+        ],
+      },
+    ],
+    moreInformationHeader: "More Information",
+    videoSrc:
+      "https://www.youtube.com/embed/Ed54iR7VmyU?si=qATEx_qJfNjzDPeT&amp;start=267",
+    videoCaption: "YouTube Video",
+    calamityVersion: "Calamity Version: 2.0.1.003",
+    terrariaVersion: "Terraria Version: 1.4.3.6",
+    timeTaken: "Time Taken: 50 seconds",
+    mnl: "MNL = 45 seconds",
+    met: "Met = True",
+    comments: "Comments: ",
+  },
+
+  {
+    bossId: "Crabulon",
+    bossName: "Crabulon",
+    bossIcon: "../bossIcons/Resize/Crabulon_map.png",
+    categories: [
+      {
+        categoryName: "Armour",
+        items: [
+          {
+            cssClass: "head",
+            imageUrl: "../armor/final/Victide_Mask.png",
+            itemName: "Victide Mask",
+          },
+          {
+            cssClass: "chest",
+            imageUrl: "../armor/final/Victide_Breastplate.png",
+            itemName: "Victide Brestplate",
+          },
+          {
+            cssClass: "legs",
+            imageUrl: "../armor/final/Victide_Greaves.png",
+            itemName: "Victide Greaves",
+          },
+        ],
+      },
+      {
+        categoryName: "Weapon(s)",
+        items: [
+          {
+            cssClass: "weapons",
+            imageUrl: "../weapons/final/Snapthorn.png",
+            itemName: "Snapthorn",
+          },
+          {
+            cssClass: "weapons",
+            imageUrl: "../weapons/final/Belladonna_Spirit_Staff.png",
+            itemName: "Belladonna Spirit Staff",
+          },
+        ],
+      },
+      {
+        categoryName: "Accessories",
+        items: [
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Spirit_Glyph.png",
+            itemName: "Spirit Glyph",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Fledgling_Wings.png",
+            itemName: "Fledgling Wings",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Wulfrum_Battery.png",
+            itemName: "Wulfrum Battery",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Feral_Claws.png",
+            itemName: "Feral Claws",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Fart_in_a_Jar.png",
+            itemName: "Fart in a Jar",
+          },
+          {
+            cssClass: "accessories",
+            imageUrl: "../accessories/final/Frostspark_Boots.png",
+            itemName: "Frostspark Boots",
+          },
+        ],
+      },
+    ],
+    moreInformationHeader: "More Information",
+    videoSrc:
+      "https://www.youtube.com/embed/rqDe5ZGxim8?si=hcOjPw1Zxd8uqqDz&amp;start=235",
+    videoCaption: "YouTube Video",
+    calamityVersion: "Calamity Version: 2.0.1.3",
+    terrariaVersion: "Terraria Version: 1.4.3.6",
+    timeTaken: "Time Taken: 34 seconds",
+    mnl: "MNL = 45 seconds",
+    met: "Met = False",
+    comments: "Comments: too fast",
+  },
+];
+
+function renderBosses() {
+  var templateScript = document.getElementById("boss-template").innerHTML;
+  var template = Handlebars.compile(templateScript);
+
+  bossesData.forEach(function (boss) {
+    var bossHtml = template(boss);
+    document.body.insertAdjacentHTML("beforeend", bossHtml);
+  });
+}
